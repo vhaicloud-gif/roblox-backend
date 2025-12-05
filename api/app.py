fad18c6ef5f88f5f8c9746f42b9946e4da38def1
vhaicloud-gif <vhaicloud@gmail.com>
2025-12-05 10:27:46 +0700

    Remove directory structure comments from app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,1 @@
-roblox-backend/
-│── database.json
-│── README.md
-│── api/
-│   ├── app.py
-│   ├── requirements.txt
-│   ├── Procfile
-│── dashboard/
-│   ├── app.py
-│   ├── requirements.txt
-│   ├── Procfile
+
